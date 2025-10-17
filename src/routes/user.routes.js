cc1c316b259866adbec8592a3e9eed80474fa8ba
beemhuse <brightawah94@gmail.com>
2025-10-17 14:47:12 +0100

    fix: user profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -478,8 +478,7 @@ router.put('/contact', updateUserContactInfo)
  *           schema:
  *             $ref: '#/components/schemas/UserProfile'
  *           example:
- *             firstName: "Tristan Peter"
- *             lastName: "Marcelino"
+ *             name: "Tristan Peter"
  *             biologicalSex: "male"
  *             preferredPronouns: "Helvim"
  *             birthday: "1990-01-01"
